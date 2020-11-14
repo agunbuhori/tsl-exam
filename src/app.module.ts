29faddce390df2ae00ece09f1679ebfeee766e5d
agunbuhori <agun.buhori@gmail.com>
2020-11-15 05:01:50 +0700

    Authentication module completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+    ConfigModule.forRoot(),
     UsersModule,
     AuthModule
   ],
